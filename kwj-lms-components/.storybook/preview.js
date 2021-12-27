c1d7572d2f9a1d5879c3cc803fac589715fd83e4
jaszczakk <smidecka324@gmail.com>
2021-12-27 10:58:21 +0700

    Initialize kwj-components

---
+++ kwj-lms-components/.storybook/preview.js
@@ -0,0 +1,41 @@
+import { ThemeProvider, createTheme } from "@mui/material";
+import { CssBaseline } from "@mui/material";
+import { typography } from "@mui/material/styles";
+import { Suspense } from "react";
+import "../src/i18n";
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+};
+
+const theme = createTheme({
+  typography: {
+    questionTitle: {
+      fontWeight: "bold",
+      fontSize: "1.5rem",
+    },
+  },
+});
+
+theme.typography.questionDescription = {
+  ...theme.typography.body2,
+};
+
+export const decorators = [
+  (Story) => {
+    return (
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Suspense fallback="Loading...">
+          <Story />
+        </Suspense>
+      </ThemeProvider>
+    );
+  },
+];
