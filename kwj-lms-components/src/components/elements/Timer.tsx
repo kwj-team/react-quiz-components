bd4aa14a0ef5ea6a4e21bd99af1c125d397600ae
jaszczakk <smidecka324@gmail.com>
2022-03-25 08:42:31 +0100

    fixed

--- kwj-lms-components/src/components/elements/Timer.tsx
+++ kwj-lms-components/src/components/elements/Timer.tsx
@@ -2,34 +2,35 @@ import { Typography } from '@mui/material';
 import { Dispatch, SetStateAction, useEffect } from 'react';
 
 interface TimerProps {
-    isActive: boolean
-    seconds: number
-    setSeconds: Dispatch<SetStateAction<number>>
+  isActive: boolean
+  seconds: number
+  setSeconds: Dispatch<SetStateAction<number>>
 }
 
 export const formatSeconds = (secs: number) => {
-    const pad = (n: number) => n < 10 ? `0${n}` : n;
+  const pad = (n: number) => n < 10 ? `0${n}` : n;
 
-    const h = Math.floor(secs / 3600);
-    const m = Math.floor(secs / 60) - (h * 60);
-    const s = Math.floor(secs - h * 3600 - m * 60);
+  const h = Math.floor(secs / 3600);
+  const m = Math.floor(secs / 60) - (h * 60);
+  const s = Math.floor(secs - h * 3600 - m * 60);
 
-    return `${pad(h)}:${pad(m)}:${pad(s)}`;
+  return `${pad(h)}:${pad(m)}:${pad(s)}`;
 };
 
 export const Timer = ({ isActive, seconds, setSeconds }: TimerProps) => {
-    useEffect(() => {
+  useEffect(() => {
+    // useEffect body
+    if (isActive) {
+      const interval = setInterval(() => {
+        setSeconds(prevSeconds => prevSeconds + 1);
+      }, 1000);
 
-        let interval: ReturnType<typeof setInterval>;
-        if (isActive) {
-            interval = setInterval(() => {
-                setSeconds(seconds => seconds + 1);
-            }, 1000);
-        }
-        return () => clearInterval(interval);
-    }, [isActive, setSeconds]);
+      // clean up function
+      return () => clearInterval(interval)
+    }
+  }, [isActive, setSeconds]);
 
-    return (
-        <Typography variant="h6" textAlign={"right"} mb={2}>Your time: {formatSeconds(seconds)}</Typography>
-    );
+  return (
+    <Typography variant="h6" textAlign={"right"} color={"red"} mb={2}>Your time: {formatSeconds(seconds)}</Typography>
+  );
 };
